0c35451d0ed424e51b5c8724eb9e3cafcab6e0fa
WolfGamer2 <rushilchopra@gmail.com>
2025-04-21 10:06:45 -0700

    Initial Commit

---
+++ Youth for Yoga/Youth_for_YogaApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Youth_for_YogaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // ← this is what launches the app's main screen :)
+        }
+    }
+}
---
+++ Youth for YogaTests/Youth_for_YogaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Youth_for_YogaTests.swift
+//  Youth for YogaTests
+//
+//  Created by Rushil Chopra on 4/21/25.
+//
+
+import Testing
+@testable import Youth_for_Yoga
+
+struct Youth_for_YogaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
