b8010c56b59ca70a064cab538d03f6e591394f58
WolfGamer2 <rushilchopra@gmail.com>
2025-05-06 12:21:31 -0700

    new changes

--- Youth for Yoga/Youth_for_YogaApp.swift
+++ Youth for Yoga/Youth_for_YogaApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct Youth_for_YogaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView() // ← this is what launches the app's main screen :)
+            ContentView() // ← this is what launches the app's main screen :) :)
         }
     }
 }
